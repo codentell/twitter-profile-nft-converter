9b95fb11655d49208085ce67186ba0387aef669e
codentell <codentell@gmail.com>
2022-10-03 21:37:36 -0700

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
+    TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET,
+    ADMIN_PRIVATE_KEY: process.env.ADMIN_PRIVATE_KEY
+  }
 }
 
 module.exports = nextConfig
